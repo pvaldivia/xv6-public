67ec40d45899ba0245b3477c25ebcdd7b736244c
“jborj006” <jborj006@ucr.edu>
2020-04-11 20:36:45 -0700

    old files and modified files

---
+++ test.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+//#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int param = atoi(argv[1]);
+    info(param);
+    //printf("Info returned: %d", num);
+    exit();
+}
